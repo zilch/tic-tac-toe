049b97af4aee5820d5218479fa5e6e371975575d
Braden Snell <bradenhs@outlook.com>
2023-05-23 15:57:04 -0400

    blue board

--- src/renderer.ts
+++ src/renderer.ts
@@ -16,7 +16,7 @@ export class Renderer {
     this.container.style.bottom = "20px";
     this.container.style.left = "20px";
     this.container.style.right = "20px";
-    this.container.style.color = "green";
+    this.container.style.color = "blue";
     document.body.appendChild(this.container);
   }
   render(params: RenderParams<State, Config>) {
