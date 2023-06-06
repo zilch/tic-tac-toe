2f3b3d4901a15b8298291630be14f509b45fcedb
Zilch Release <>
2023-06-06 21:36:49 +0000

    v0.0.8

--- out/config.js
+++ out/config.js
@@ -1,4 +1,4 @@
-var o={type:"object",required:["startingPosition"],properties:{startingPosition:{description:"The best description that ever there was.",type:"array",minItems:3,maxItems:3,items:{type:"array",minItems:3,maxItems:3,items:{type:["string"],enum:["x","o","empty"]}}}}};Zilch.configSchema=o;Zilch.configPresets=[{name:"Standard",value:`{
+var a={type:"object",required:["startingPosition"],properties:{startingPosition:{description:"The best description that ever there was.",type:"array",minItems:3,maxItems:3,items:{type:"array",minItems:3,maxItems:3,items:{type:["string"],enum:["x","o","empty"]}}}}};Zilch.configSchema=a;Zilch.configPresets=[{name:"Standard",value:`{
   // 3x3 matrix with values "x", "o" and "empty"
   "startingPosition": [
     ["empty", "empty", "empty"],
@@ -6,5 +6,5 @@ var o={type:"object",required:["startingPosition"],properties:{startingPosition:
     ["empty", "empty", "empty"]
   ]
 }
-`}];Zilch.parseConfig=t=>{let n=0,e=0,i=t.startingPosition.map(a=>a.map(r=>r==="x"?n++*2:r==="o"?e++*2+1:null));if(n-e!==0&&n-e!==1)throw new Error("There should be an even number of Xs and Os (or just one more X than O) in the starting position.");return{initialBoard:i,initialTurn:e+n}};Zilch.serializeConfig=t=>t.initialBoard.map(e=>e.map(i=>i===null?"empty":i%2===0?"x":"o")).map(e=>e.join(",")).join("|");Zilch.summarizeConfig=t=>t.initialTurn===0?"standard starting position":"custom starting position";
+`}];Zilch.parseConfig=e=>{let t=0,i=0,r=e.startingPosition.map(o=>o.map(n=>n==="x"?(t++,"x"):n==="o"?(i++,"o"):"empty"));if(t-i!==0&&t-i!==1)throw new Error("There should be an even number of Xs and Os (or just one more X than O) in the starting position.");return{initialBoard:r,initialTurn:i+t}};Zilch.serializeConfig=e=>e.initialBoard.map(t=>t.join(",")).join("|");Zilch.summarizeConfig=e=>e.initialTurn===0?"standard starting position":"custom starting position";
 //# sourceMappingURL=config.js.map
