48e561cc30e1d7feac618b72423341f918586f12
Braden Snell <bradenhs@outlook.com>
2023-06-23 14:27:35 -0400

    updates for latest

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,14 @@
 import type { CreateBotParams } from "zilch-game-engine";
 
-Zilch.Bot = class {
+Zilch.Bot = class Bot {
   params: CreateBotParams;
 
   constructor(params: CreateBotParams) {
     this.params = params;
+  }
+
+  static create(params: CreateBotParams) {
+    return new Bot(params);
   }
 
   move(payload: string) {
